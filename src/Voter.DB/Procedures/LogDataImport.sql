da2e6c01d43ef7281a1d8a7bf0a6894c7fb48cd7
robert <robertbuchanan@gci.net>
2018-10-28 12:02:14 -0800

    1. Added class library for data. --> Probably using EF6 with the revers poco tool form Simon Hughes. 2. Added stubs for more data import procedures    a. LogDataImport    b. ParseRawToVoterElections    c.  ParseRawToVoters

---
+++ src/Voter.DB/Procedures/LogDataImport.sql
@@ -0,0 +1,11 @@
+﻿/******************************************************************************
+    
+    Description:    STUB HEADER
+
+*******************************************************************************/
+CREATE PROCEDURE [dbo].[LogDataImport]
+    @param1 int = 0,
+    @param2 int
+AS
+    SELECT @param1, @param2
+RETURN 0
---
+++ src/Voter.DB/Procedures/ParseRawToVoterElections.sql
@@ -0,0 +1,11 @@
+﻿/******************************************************************************
+    
+    Description:    STUB HEADER
+
+*******************************************************************************/
+CREATE PROCEDURE [dbo].[ParseRawToVoterElections]
+    @param1 int = 0,
+    @param2 int
+AS
+    SELECT @param1, @param2
+RETURN 0
